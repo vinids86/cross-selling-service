0288eaf2f568953ef0310e640c5942edadc0db7f
Vinicius.saraiva Acc <viniciusdsaraiva@gmail.com>
2018-01-13 18:50:20 -0200

    Add Zipkin trace

--- src/main/java/br/com/makeshiftonjava/crosssellingservice/application/CrossSellingController.java
+++ src/main/java/br/com/makeshiftonjava/crosssellingservice/application/CrossSellingController.java
@@ -1,6 +1,8 @@
 package br.com.makeshiftonjava.crosssellingservice.application;
 
 import br.com.makeshiftonjava.crosssellingservice.model.CrossSelling;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cross-selling")
 public class CrossSellingController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CrossSellingController.class);
+
     @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
     public ResponseEntity<CrossSelling> retrieveCrossSeling(@PathVariable Long productId) {
+        LOG.info("cross-selling/" + productId);
         return ResponseEntity.ok(new CrossSelling(productId));
     }
 }
